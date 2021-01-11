72fe8a8feea4a84dcf00ec7062df9c0c53119b77
david <david.gaillet@live.fr>
2021-01-11 14:46:47 +0100

    Debut model Global + creation projet api

---
+++ Api_ModelGobal/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api_ModelGobal.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Passwd { get; set; }
+        public string LasName { get; set; }
+        public string FirstName { get; set; }
+
+    }
+}
